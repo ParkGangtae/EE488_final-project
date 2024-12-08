d8cfa0338bc6f742efabe462a8f5c7a460cdc2cf
dugongtae <51894747+ParkGangtae@users.noreply.github.com>
2024-12-08 16:24:07 +0900

    Delete models/ViT.py

--- models/ViT.py
+++
@@ -1,7 +0,0 @@
-# main.py
-
-from vision_transformer import VisionTransformer
-
-def MainModel(nOut=256, **kwargs):
-    # 커스텀 Vision Transformer 모델
-    return VisionTransformer(img_size=224, patch_size=32, in_channels=3, num_classes=nOut)
